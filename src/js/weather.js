d00705816d79bddb5ca79821f986dc28e303d9b4
Gagan Malvi <malvigagan@gmail.com>
2022-11-07 19:22:25 +0530

    legendary-feat: finish the site.
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

---
+++ src/js/weather.js
@@ -0,0 +1,49 @@
+// Copyright (C) 2022 Gagan Malvi
+// SPDX-License-Identifier: Apache-2.0
+
+const api = {
+    key: "cd3c097765ba92903ec783b6cf5bef86",
+    base: "https://api.openweathermap.org/data/2.5/"
+}
+
+function timeConverter(UNIX_timestamp){
+    var a = new Date(UNIX_timestamp * 1000);
+    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    var year = a.getFullYear();
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    var hour = a.getHours();
+    var min = a.getMinutes();
+    var sec = a.getSeconds();
+    var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+    return time;
+}
+
+function getTemperature() {
+    document.querySelector('.weatherup').style.display = "none";
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            let lat = position.coords.latitude;
+            let lon = position.coords.longitude;
+            fetch(`${api.base}weather?lat=${lat}&lon=${lon}&units=metric&APPID=${api.key}`)
+             .then(weather => weather.json()).then((output) => {
+                document.querySelector('.weatherup').style.display = "flex";
+                console.log(output);
+                document.querySelector('.weather-icon').src = `https://openweathermap.org/img/wn/${output.weather[0].icon}@2x.png`;
+                document.querySelector('.temp').innerText = `${output.main.temp}`;
+                document.querySelector('.city').innerText = `${output.name}, ${output.sys.country}`;
+                document.querySelector('.desc').innerText = `${output.weather[0].main}`;
+                document.querySelector('.wind').innerText = `${output.wind.speed} km/h`;
+                document.querySelector('.hum').innerText = `${output.main.humidity}%`;
+                document.querySelector('.pres').innerText = `${output.main.pressure} hPa`;
+                document.querySelector('.min').innerText = `${output.main.temp_min} C`;
+                document.querySelector('.max').innerText = `${output.main.temp_max} C`;
+                document.querySelector('.vis').innerText = `${output.visibility} m`;
+                document.querySelector('.sunrise').innerText = `${timeConverter(output.sys.sunrise)}`;
+                document.querySelector('.sunset').innerText = `${timeConverter(output.sys.sunset)}`;
+            });
+        });
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
+}
